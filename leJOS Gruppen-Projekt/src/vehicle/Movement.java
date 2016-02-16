bbb801bfd8421e928ccd834b247e6741c2d736cf
Zenneo <agrar.pfurtz@gmail.com>
2016-02-16 17:17:21 +0100

    Adding debug messages to Movement.java
    Similar to a272baf91b1d4762a58c30a3702298058e302f6c

--- leJOS Gruppen-Projekt/src/vehicle/Movement.java
+++ leJOS Gruppen-Projekt/src/vehicle/Movement.java
@@ -1,6 +1,7 @@
 package vehicle;
 
 import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.comm.RConsole;
 
 public class Movement {
 	
@@ -16,6 +17,9 @@ public class Movement {
 	 * @return the vehicle_movement
 	 */
 	public int getVehicle_movement() {
+		//DEBUG MSG
+		RConsole.println("STATUS: Current movement: " + vehicle_movement);
+		
 		return vehicle_movement;
 	}	
 	
@@ -37,6 +41,9 @@ public class Movement {
 	}
 
 	public void checkMovement() {
+		//DEBUG MSG
+		RConsole.println("ACTION: Checking vehicle movement... ");
+		
 		if (!engine.isMoving() ) {
 			vehicle_movement = 1; // still
 		} else if (engine.getRotationSpeed() > 0 ) { 
@@ -48,9 +55,17 @@ public class Movement {
 	
 	public void moveToStation(int station) {
 		if (station == 1) {
+			//DEBUG MSG
+			RConsole.println("TASK: Move to station" + station);
+			
 			// TODO Move vehicle to station
+			forward();
 		} else if (station == 2) {
+			//DEBUG MSG
+			RConsole.println("TASK: Move to station" + station);
+			
 			// TODO Move vehicle to station
+			backward();
 		} else {
 			throw new UnsupportedOperationException();
 		}
@@ -58,16 +73,23 @@ public class Movement {
 	}
 	
 	public void stop() {
+		//DEBUG MSG
+		RConsole.println("ACTION: Stopping vehicle... ");
+		
 		engine.stop(true);
 		vehicle_movement = 1;
 	}
 	
 	public void forward() {
+		//DEBUG MSG
+		RConsole.println("ACTION: Moving vehicle forward... ");
 		engine.forward();
 		vehicle_movement = 2;
 	}
 	
 	public void backward() {
+		//DEBUG MSG
+		RConsole.println("ACTION:Moving vehicle backward... ");
 		engine.backward();
 		vehicle_movement = 3;
 	}
