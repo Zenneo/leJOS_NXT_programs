2cd6d0bb269b75b63091420ee4521e2081ce4f45
Zenneo <agrar.pfurtz@gmail.com>
2016-02-27 14:29:52 +0100

    Restructured code; changed basic functionality

--- leJOS Gruppen-Projekt/src/arm_test/Arm_rotate.java
+++ leJOS Gruppen-Projekt/src/arm_test/Arm_rotate.java
@@ -11,8 +11,10 @@ public class Arm_rotate {
 	private static NXTRegulatedMotor motor = Motor.B;
 
 	// runtime integers
-	private static int rotation_tick = 1; // degree
 	private static int wait_between_press = 1000; // in milliseconds
+	private static int speed; // rotation speed
+	private static int acceleration; // motor acceleration
+	private static int rotateTarget = 0;
 
 	public static void main(String[] args) {
 		try {
@@ -22,76 +24,88 @@ public class Arm_rotate {
 			e.printStackTrace();
 		}
 
-		// initial setup
-		long pressed_since = System.currentTimeMillis();
-
-		// initial screen
-		LCD.drawString("Intervall:", 0, 0);
-		LCD.drawString("Confirm with", 0, 4);
-		LCD.drawString("orange.", 0, 5);
-		while (!Button.ENTER.isDown()) {
-			LCD.clear(1);
-			LCD.drawString("<< " + rotation_tick + " >>", 0, 1);
-			if (Button.LEFT.isUp() && Button.RIGHT.isUp()) {
-				pressed_since = 0;
-			} else if (Button.LEFT.isDown() || Button.RIGHT.isDown()) {
-				if (pressed_since == 0) {
-					pressed_since = System.currentTimeMillis();
-					change_tick();
-				} else if (System.currentTimeMillis() - pressed_since > wait_between_press) {
-					change_tick();
-				}
-			}
-		}
-
-		// main screen
-		LCD.clear();
-		LCD.drawString("Left =negative", 0, 0);
-		LCD.drawString("Right=positive", 0, 1);
-		LCD.drawString("Intervall: " + rotation_tick, 0, 2);
-		LCD.drawString("Current angle:", 0, 4);
-		LCD.drawInt(motor.getPosition(), 0, 5);
+		// screen: ask for speed
+		speed = askForValue("Speed", 500, 25, wait_between_press);
+		
+		// screen: ask for acceleration
+		acceleration = askForValue("Acceleration", 1000, 50, wait_between_press);
+		
+		
+		// set determined values
+		motor.setSpeed(speed);
+		motor.setAcceleration(acceleration);
+		
 
 		// main loop
 		while (Button.ESCAPE.isUp()) {
+			LCD.clear();
+			LCD.drawString("Speed: " + speed, 0, 1);
+			LCD.drawString("Accel: " + acceleration, 0, 2);
+			LCD.drawString("Cur angle: " + motor.getPosition(), 0, 3);
+			
+			if (motor.getPosition() == rotateTarget) {
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				rotateTarget = askForValue("Tar angle", motor.getPosition(), 1, wait_between_press);
+				motor.rotateTo(rotateTarget);
+			} else {
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+
+	}
+
+	
+	private static int askForValue(String name, int initial, int step, int waitBetween) {
+		LCD.clear();
+		LCD.drawString(name + ":", 0, 0);
+		LCD.drawString("Confirm with", 0, 3);
+		LCD.drawString("orange.", 0, 4);
+		
+		int value = initial;
+		long pressed_since = 0;
+		
+		while (!Button.ENTER.isDown()) {
+			LCD.clear(1);
+			LCD.drawString("<< " + value + " >>", 0, 1);
 			if (Button.LEFT.isUp() && Button.RIGHT.isUp()) {
 				pressed_since = 0;
 			} else if (Button.LEFT.isDown() || Button.RIGHT.isDown()) {
 				if (pressed_since == 0) {
 					pressed_since = System.currentTimeMillis();
-					rotate();
-				} else if (System.currentTimeMillis() - pressed_since > wait_between_press) {
-					rotate();
+					if (Button.LEFT.isDown() && Button.RIGHT.isUp() && value > 0) {
+						value = value - step;
+					} else if (Button.LEFT.isUp() && Button.RIGHT.isDown()) {
+						value = value + step;
+					}
+				} else if (System.currentTimeMillis() - pressed_since > waitBetween) {
+					if (Button.LEFT.isDown() && Button.RIGHT.isUp() && value > 0) {
+						value = value - step;
+					} else if (Button.LEFT.isUp() && Button.RIGHT.isDown()) {
+						value = value + step;
+					}
+					try {
+						Thread.sleep(175);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 		}
-
-	}
-
-	private static void rotate() {
-		if (Button.LEFT.isDown() && Button.RIGHT.isUp()) {
-			motor.rotate(0 - rotation_tick);
-		} else if (Button.LEFT.isUp() && Button.RIGHT.isDown()) {
-			motor.rotate(rotation_tick);
-		}
-		LCD.clear(5);
-		LCD.drawInt(motor.getPosition(), 0, 5);
-	}
-
-	private static void change_tick() {
-		if (Button.LEFT.isDown() && Button.RIGHT.isUp()) {
-			if (rotation_tick > 0) {
-				rotation_tick--;
-			}
-		} else if (Button.LEFT.isUp() && Button.RIGHT.isDown()) {
-			rotation_tick++;
-		}
-		try {
-			Thread.sleep(250);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		
+		return value;
 	}
 
 }
