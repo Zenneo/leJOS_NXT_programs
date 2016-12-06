407469ffebf78eb0a2c598619fdf453bf14d4cf9
chunkleader <chunkleader@gmail.com>
2016-12-06 14:18:49 +0100

    added boxdetect to the comment branch

---
+++ leJOS Gruppen-Projekt/src/rotationStation/BoxDetect.java
@@ -0,0 +1,22 @@
+package rotationStation;
+
+import lejos.nxt.SensorPort;
+import lejos.nxt.UltrasonicSensor;
+
+public class BoxDetect {
+	private final UltrasonicSensor sensor;
+	
+	public BoxDetect(SensorPort port) {
+		sensor = new UltrasonicSensor(port);
+	}
+	
+	public synchronized boolean isBox() {
+		int distance = sensor.getDistance();
+		
+		if (distance < 40) { // distance value has to be measured!
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
